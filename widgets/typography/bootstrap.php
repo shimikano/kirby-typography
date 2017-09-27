eafc645aeba50a38c178b966cb8d55e3e9d1d7b7
Yoshimi Takano <shimikano@users.noreply.github.com>
2017-09-27 17:03:33 +0200

    DRY for the cache status.

--- widgets/typography/bootstrap.php
+++ widgets/typography/bootstrap.php
@@ -8,7 +8,7 @@ use Kirby\Plugins\Typography\KirbyTypography;
 // until the Kirby panel fully supports localization.
 function kirby_typography_widget_get_translations( $code = null ) {
   $language_directory = __DIR__ . DS . 'languages';
-  
+
   if ($code !== 'en') {
     if (!preg_match('/^[a-z]{2}([_-][a-z0-9]{2,})?$/i', $code) ||
         !file_exists($language_directory . DS . $code . '.php')) {
@@ -16,7 +16,7 @@ function kirby_typography_widget_get_translations( $code = null ) {
       $code = 'en';
     }
   }
-  
+
   return require($language_directory . DS . $code . '.php');
 }
 
@@ -24,31 +24,35 @@ $kirby->set('widget', 'typography', __DIR__ );
 $kirby->set('route', [
   'pattern' => 'plugins/typography/widget/api/(:any)',
   'action'  => function($action) {
-    
+
     $user = site()->user()->current();
     if (!$user || !$user->isAdmin()) {
       return Response::error('Only administrators can access the widget API of the typography plugin.', 401);
     }
-    
+
     $l = kirby_typography_widget_get_translations($user->language());
-    
+
+    function status($l) {
+      $data = Cache::instance()->status();
+
+      return Response::success(sprintf($l['typography.widget.cache.status'], round($data['size'] / 1024, 2), $data['files']), $data);
+    }
+
     switch ($action) {
       case 'status';
-        $data = Cache::instance()->status();
-        return Response::success(sprintf($l['typography.widget.cache.status'], round($data['size'] / 1024, 2), $data['files']), $data);
-        break;
-      
+        return status($l);
+
       case 'flush':
         $success = Cache::instance()->flush();
+
         if ($success) {
-          $data = Cache::instance()->status();
-          return Response::success(sprintf($l['typography.widget.cache.status'], round($data['size'] / 1024, 2), $data['files']), $data);
+          return status($l);
         } else {
           // Sending a success message here, although it is an error message,
           // but our wiget just displays the text and should not care.
           return Response::success($l['typography.widget.cache.error']);
         }
-        
+
       default:
         return Response::error('Invalid plugin action. The action "' . html($action) . '" is not defined.');
     }
